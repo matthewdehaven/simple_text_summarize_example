4389431a9975cb0a8c773d15cf0a71d00e578809
Matthew DeHaven <matthew.dehaven@outlook.com>
2023-07-16 14:43:13 -0500

    Add outputs

--- summarize.py
+++ summarize.py
@@ -11,9 +11,11 @@ with open("example.txt") as f:
     for line in f:
         text += line
 
-print(text)
+# print(text)
 input_ids = tokenizer([text], max_length=1028, truncation=True, return_tensors="pt").input_ids
 with torch.no_grad():
     generated_ids = model.generate(input_ids=input_ids, num_beams=2, max_new_tokens=128,  repetition_penalty=2.5, length_penalty=1.0, early_stopping=True)
     summarization = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+
+
 print(summarization)
